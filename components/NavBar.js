7ba3bb5024c64441717ce73012e3c46a6264bb01
DESKTOP-ND1JTB6\Sujin <sujin_kim2@tmax.co.kr>
2022-11-02 17:55:21 +0900

    first commit

---
+++ components/NavBar.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import styles from "./NavBar.module.css";
+
+export default function NavBar() {
+  const router = useRouter();
+  console.log(router);
+  return (
+    <nav className={styles.nav}>
+      <Link
+        style={{ color: router.pathname === "/" ? "red" : "blue" }}
+        href="/"
+      >
+        Home
+      </Link>
+      <Link
+        style={{ color: router.pathname === "/about" ? "red" : "blue" }}
+        href="/about"
+      >
+        About
+      </Link>
+    </nav>
+  );
+}
